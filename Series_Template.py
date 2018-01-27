028229a057b635df280853043b58f4dfd75210cf
Cross the Mage <6815252+CrossCV@users.noreply.github.com>
2018-01-28 05:21:41 +0800

    Update Series_Template.py
    改成能用的版本了
    能正常报范围重叠的错误，语法错误需要商量一下怎么规定

--- Series_Template.py
+++ Series_Template.py
@@ -3,7 +3,7 @@
 import random
 import re
 
-INF = 1000000
+INF = 1000000.0
 
 
 class Unit_Template:
@@ -40,57 +40,64 @@ class Unit_Template:
             a = func()
         return a
 
-
-def getResult(ptn):
-    class RangeExcept(Exception):
-        def __init__(self, args):
-            self.args = args
-
-    rule1 = r'(\d+)'
-    rule2 = r'(\[(\d+)\,(\d+)\]([A-Z])\((\d+)\))'
-    rule3 = r'(\[(\d+)\,(\d+)\])'
-    result = []
-    curBound = -INF
-
-    try:
-        while ptn:
-            ptrn = re.match(r'(' + rule1 + r'|' + rule2 + r'|' + rule3 + r')', ptn).group(0)
-            ptn = ptn[len(ptrn) + 1:]
-            if re.match(rule1, ptrn):
-                lower_bound = float(re.search(rule1, ptrn).group(0))
-                if curBound > lower_bound:
-                    raise RangeExcept("重叠！")
-                else:
-                    curBound = lower_bound
-                a = Unit_Template(lower_bound)
-                result.append(a.distribute())
-            elif re.match(rule2, ptrn):
-                lower_bound = float(re.search(rule2, ptrn).group(2))
-                upper_bound = float(re.search(rule2, ptrn).group(3))
-                if (curBound > lower_bound) | (lower_bound > upper_bound):
-                    raise RangeExcept("重叠！")
-                else:
-                    curBound = lower_bound
-                typ = re.search(rule2, ptrn).group(4)
-                arg = float(re.search(rule2, ptrn).group(5))
-                a = Unit_Template(lower_bound, upper_bound, "gauss", arg)
-                result.append(a.distribute())
-            elif re.match(rule3, ptrn):
-                lower_bound = float(re.search(rule3, ptrn).group(2))
-                upper_bound = float(re.search(rule3, ptrn).group(3))
-                if (curBound > lower_bound) | (lower_bound > upper_bound):
-                    raise RangeExcept("重叠！")
-                else:
-                    curBound = lower_bound
-                a = Unit_Template(lower_bound, upper_bound)
-                result.append(a.distribute())
-    except RangeExcept:
+class Series_Template:
+    def __init__(self,ptn):
+        class RangeExcept(Exception):
+            def __init__(self, args):
+                self.args = args
+        rule1 = r'(\d+)'
+        rule2 = r'(\[(\d+)\,(\d+)\]([A-Z])\((\d+)\))'
+        rule3 = r'(\[(\d+)\,(\d+)\])'
         result = []
-        print("范围重叠！")
-        return None
-    return result
+        curBound = -INF
+        try:
+            while ptn:
+                ptrn = re.match(r'(' + rule1 + r'|' + rule2 + r'|' + rule3 + r')', ptn).group(0)
+                ptn = ptn[len(ptrn) + 1:]
+                if re.match(rule1, ptrn):
+                    lower_bound = float(re.search(rule1, ptrn).group(0))
+                    if curBound > lower_bound:
+                        raise RangeExcept("重叠！")
+                    else:
+                        curBound = lower_bound
+                    a = Unit_Template(lower_bound)
+                    result.append(a)
+                elif re.match(rule2, ptrn):
+                    lower_bound = float(re.search(rule2, ptrn).group(2))
+                    upper_bound = float(re.search(rule2, ptrn).group(3))
+                    if (curBound > lower_bound) | (lower_bound > upper_bound):
+                        raise RangeExcept("重叠！")
+                    else:
+                        curBound = lower_bound
+                    typ = re.search(rule2, ptrn).group(4)
+                    arg = float(re.search(rule2, ptrn).group(5))
+                    a = Unit_Template(lower_bound, upper_bound, "gauss", arg)
+                    result.append(a)
+                elif re.match(rule3, ptrn):
+                    lower_bound = float(re.search(rule3, ptrn).group(2))
+                    upper_bound = float(re.search(rule3, ptrn).group(3))
+                    if (curBound > lower_bound) | (lower_bound > upper_bound):
+                        raise RangeExcept("重叠！")
+                    else:
+                        curBound = lower_bound
+                    a = Unit_Template(lower_bound, upper_bound)
+                    result.append(a)
+        except RangeExcept:
+            result = []
+            print("范围重叠！")
+            self.seires=None
+        self.series=result
+    def distribute(self):
+        result=[]
+        for a in self.series:
+            result.append(a.distribute())
+        return result
 
 
 if __name__ == '__main__':
     ptn = input("Please enter a pattern: ")
-    print(getResult(ptn))
+    a = Series_Template(ptn)
+    print(a.distribute())
+    print(a.distribute())
+    print(a.distribute())
+    print(a.distribute())
